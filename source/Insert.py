5e4ffd6596b9249f963e824fb0ae918852cc85e3
robert bjornson <robert.bjornson@yale.edu>
2019-03-04 16:52:47 +0000

    new insert and query scripts

--- source/Insert.py
+++ source/Insert.py
@@ -1,15 +1,8 @@
-'''
-zInsert.py
-Loads tab-separated text file data onto an existing data stream on an existing multichain
-
-
-Usage: $ python zInsert.py <tab-separated-data>.txt <chainName> <streamName>
-Example: python zInsert.py training1.txt testChain testStream
-
-modified by GG on November 4th, 2018
 
 '''
-
+columns=['TIMESTAMP','NODE','ID','REFID','USER','ACTIVITY','RESOURCE']
+1522000002801	1	1	1	1	REQ_RESOURCE	MOD_UCSC_Genome_Bioinformatics
+'''
 
 import sys
 import subprocess
@@ -17,35 +10,30 @@ import time
 
 #	Set variables, open file, split on new lines
 
-lines = open(sys.argv[1]).read().splitlines()
+cols=['T','N','I','F','U','A','R']
+
+fn = sys.argv[1]
 chainName=sys.argv[2]
 streamName=sys.argv[3]
 
-#	Replace tabs with '\t' character (necessary for multichain--no spaces allowed on multichain keys)
+logf=open("Insert.log", 'w+')
 
-def removeTabs(lines):
-	newlines=[]
-	for line in range(0, len(lines)):
-		str=lines[line]
-		str=" ".join(str.split())
-		str=str.replace(' ', '\\t')
-		newlines.append(str)
-	return newlines
+# first count the lines
+lines=0
+for l in open(fn):
+    lines+=1
 
-#newlines=removeTabs(lines)
+# now for real
+for i, l in enumerate(open(fn)):
+     vals = l.strip().split()
+     timestamp=vals[0]
+     rec=':'.join(vals)
+     cmd='multichain-cli {} publish {} {} {}'.format(chainName, streamName, timestamp, rec.encode('hex'))
+     txid=subprocess.check_output(cmd, shell=True, stderr=logf)
+     for c, v in zip(cols,vals):
+         cmd='multichain-cli {} publish {} {}:{} {}'.format(chainName, streamName, c, v, txid)
+         dummy=subprocess.check_output(cmd, shell=True, stderr=logf)
 
-#	Use subprocess to load lines of text to chain
+     sys.stdout.write('\r{0:12}:{1:12}'.format(i, lines))
+     sys.stdout.flush()
 
-for line in range(0, len(lines)):
-        ss=lines[line]
-        ss=" ".join(ss.split())
-        ss=ss.replace(' ','\\t')
-        newlines=ss
-        subprocess.call(['multichain-cli', str('{}'.format(chainName)), 'publish',str('{}'.format(streamName)), str('{}'.format(newlines)),str('{}'.format(str(line).encode('hex')))])
-
-
-import os
-import psutil
-process = psutil.Process(os.getpid())
-print('\n\n Total memory in bytes:\n\n')
-print(process.memory_info().rss)
--- source/Query.py
+++ source/Query.py
@@ -1,3 +1,9 @@
+
+'''
+to do:
+- handle no input? 
+'''
+
 '''
 zQuery.py
 Queries an existing stream from a loaded multichain chain.
@@ -47,166 +53,91 @@ python zFunctionalQuery.py --help
 
 
 import argparse
+import sys
+import os
 import subprocess
 import json
 import pandas as pd
 
+columns=['TIMESTAMP','NODE','ID','REFID','USER','ACTIVITY','RESOURCE']
+
 	################### Parses user-supplied arguments ###################
 parser=argparse.ArgumentParser()
+# required multichain options
 parser.add_argument('-cn','--chainName', dest='chainName', required=True, help='The name of the chain to be queried')
 parser.add_argument('-sn','--streamName', dest='streamName', required=True, help='The name of the data stream to be queried')
-parser.add_argument('-st','--startTime'.upper(), dest='startTime',default=None, help='Unix timestamp start time of query')
-parser.add_argument('-et','--endTime'.upper(), dest='endTime',default=None, help='Unix timestamp end time of query')
+
+# timestamp filtering
+parser.add_argument('-st','--startTime', type=int, dest='startTime',default=-sys.maxint, help='Unix timestamp start time of query')
+parser.add_argument('-et','--endTime', type=int, dest='endTime',default=sys.maxint, help='Unix timestamp end time of query')
+
+# things to query on
 parser.add_argument('-n','--node'.upper(),'--node', dest='node',default=None,help='Node')
 parser.add_argument('-i','--id'.upper(),'--id',dest='id',default=None,help='id')
-parser.add_argument('-rd','--refid'.upper(),'--refid',dest='refid',default=None,help='refid')
+parser.add_argument('-f','--refid'.upper(),'--refid',dest='refid',default=None,help='refid')
 parser.add_argument('-u','--user'.upper(),'--user',dest='user',default=None, help='user')
 parser.add_argument('-a','--activity'.upper(),'--activity',dest='activity',default=None,help='activity')
 parser.add_argument('-r','--resource'.upper(), '--resource',dest='resource',default=None,help='resource')
+parser.add_argument('-t','--timestamp'.upper(), dest='timestamp', required=False, help='A single timestamp')
+
+# output ordering
 parser.add_argument('-sb','--sortBy',dest='sortBy',default=None,help='Sort data by a specific column')
-parser.add_argument('-ob','--orderBy',dest='orderBy',default=None,help='Sort data in ascending or descending order')
-parser.add_argument('-ts','--timestamp'.upper(), dest='timestamp', required=False, help='A single timestamp')
+parser.add_argument('-ob','--orderBy',dest='orderBy',default='asc',help='Sort data in ascending or descending order')
 
+def Query(qstr):
+        subqueries=[]
+        o=parser.parse_args(qstr)
+        if o.timestamp: subqueries.append('T:{}'.format(o.timestamp))
+        if o.node:      subqueries.append('N:{}'.format(o.node))
+        if o.id:        subqueries.append('I:{}'.format(o.id))
+        if o.refid:     subqueries.append('F:{}'.format(o.refid))
+        if o.user:      subqueries.append('U:{}'.format(o.user))
+        if o.activity:  subqueries.append('A:{}'.format(o.activity))
+        if o.resource:  subqueries.append('R:{}'.format(o.resource))
 
-o=parser.parse_args()
+        if not subqueries:
+                print("Error: you must provide at least one query element")
+                sys.exit(1)
 
+        logf=open("Query.log", "w+")
+        txids=[]
+        for sq in subqueries:
+                cmd='multichain-cli {} liststreamkeyitems {} {} false 100000'.format(o.chainName, o.streamName, sq)
+                recs=json.loads(subprocess.check_output(cmd, shell=True, stderr=logf))
+                # txids is a list of sets, each holding all the txids found for that query, e.g. -u 9
+                txids.append(set([r['data'] for r in recs]))
+        matching_txids=reduce(set.intersection, txids)
+        results=[]
+        for txid in matching_txids:
+                cmd='multichain-cli {} getstreamitem {} {}'.format(o.chainName, o.streamName, txid)
+                rec=json.loads(subprocess.check_output(cmd, shell=True, stderr=logf))
+                flds=rec['data'].decode("hex").split(":")
+                timestamp=int(flds[0])
+                if o.startTime <= timestamp and timestamp <= o.endTime:
+                        results.append(flds)
 
-class Chain():
-
-        def __init__(self, o):
-	###################Initialize the Chain object###################
-                self.chainName=o.chainName
-                self.streamName=o.streamName
-		self.startTime=o.startTime
-		self.endTime=o.endTime
-		self.node=o.node
-                self.id=o.id
-		self.refid=o.refid
-                self.user=o.user
-                self.activity=o.activity
-                self.resource=o.resource
-		self.timestamp=o.timestamp
-		self.dict={k:v for k,v in o._get_kwargs() if v != None}
-		self.noneDict={k:v for k,v in o._get_kwargs()}
-		if o.sortBy==None:
-			self.sortBy='TIMESTAMP'
-		else:
-			self.sortBy=o.sortBy.upper()	
-		if o.orderBy==None or o.orderBy.upper() in [True, 'ASC','ASCENDING']:
-			self.orderBy=True
-		else:
-			self.orderBy=False
-	################### Create the dataframe ###################
-		columns=['TIMESTAMP','NODE','ID','REFID','USER','ACTIVITY','RESOURCE']
-		array=[]
-		string='multichain-cli {} liststreamkeys {}'.format(self.chainName, self.streamName)
-		rawItems=rawItems=subprocess.check_output([i for i in string.split()])
-		#rawItems=subprocess.check_output(['multichain-cli',str('{}'.format(self.chainName)),'liststreamitems',str('{}'.format(self.streamName)), 'false','10000000'])	
-		items=json.loads(str(rawItems))
-		for item in range(0, len(items)):
-			array.append(items[item]['key'])
-		df=pd.DataFrame([sub.split('\t') for sub in array])
-		df.columns=columns
-		df=df.applymap(lambda x: x.upper())
-		for key in ['TIMESTAMP']:
-			df[key]=map(lambda x: int(x), df[key])
-		self.df=df
-		
-	################### Examples to help user with syntax ###################	
-	def examples(self):
-		print('\n Field options for querying: {}\n'.format(o.__dict__.keys()))
-		print('\n Syntax: rows.query(\'example string\')\n ')
-		print('\n Example: rows.query(\'user 1 node 1\')\n ')
-			
-	################### Called by program (does not require interactive mode) ###################
-	def firstQuery(self):
-		inputDict=self.dict
-		unQueriables=['chainName','streamName','sortBy','orderBy']
-		queryDict=self.dict
-		for k in unQueriables:
-			if k in queryDict.keys():
-				del queryDict[k]
-		if queryDict=={}:
-			print('\n No matches (or no field queried). Returning truncated full chain. If in interactive mode, please use rows.examples() \n')
-			return self.df.sort_values(by='{}'.format(self.sortBy.upper()), ascending=self.orderBy)
-		newValues=['{0}=={1}{2}{1}'.format(key.upper(),'\\\'',value.upper()) for key,value in queryDict.items()]
-		newDict=dict(zip(queryDict.keys(),newValues))
-		if 'startTime' in queryDict.keys() and 'endTime' in queryDict.keys():
-			newDict['TIMESTAMP']='{0}<=TIMESTAMP<={1}'.format(queryDict['startTime'],queryDict['endTime'])
-			del newDict['startTime']
-			del newDict['endTime']
-		if 'startTime' in queryDict.keys() and 'endTime' not in queryDict.keys():
-			newDict['TIMESTAMP']='{0}<=TIMESTAMP<={1}'.format(queryDict['startTime'],queryDict['startTime'])
-			del newDict['startTime']
-		if 'endTime' in queryDict.keys() and 'startTime' not in queryDict.keys():
-			newDict['TIMESTAMP']='{0}<=TIMESTAMP<={1}'.format(queryDict['endTime'],queryDict['endTime'])
-			del newDict['endTime']
-		array=['{}'.format(newDict.values()[i]) for i in range(0, len(newDict.values()))]
-		seperator=' and '
-		queryStr=seperator.join(array)
-		queryStr='\'{}\''.format(queryStr)
-		result=self.df.query('{}'.format(eval(queryStr)))
-		#print('Check it out with interactive mode, multiple queries can be performed on this instance of the chain. Please use rows.examples() in interactive mode to see examples.')
-		return result.sort_values(by='{}'.format(self.sortBy), ascending=self.orderBy)
-
-
-	################### For interactive mode queries ###################	
-	def query(self,str):
-		queriables=['startTime','endTime','node','id','refid','user','activity','resource']
-		sortables={'*timestamp':'TIMESTAMP','*node':'NODE','*id':'ID','*refid':'REFID','*user':'USER','*activity':'ACTIVITY','*resource':'RESOURCE'}
-		orderables=['-d','--descending','des','DES','DESCENDING','descending']
-		queryOptionsDict={k:None for k in queriables}
-		k=[x for x in str.split() if x in queryOptionsDict.keys()]
-		v=[y for y in str.split() if y not in queryOptionsDict.keys()]
-		d=dict(zip(k,v)) #Our query dictionary
-		sortByArray=[x for x in str.split() if x in sortables.keys()]
-		#print sortByArray
-		orderBy=[x for x in str.split() if x in orderables]
-		if orderBy!=[]:
-			orderBy=False
-		else:
-			orderBy=True
-		if sortByArray!=[]:
-			sortBy=sortables[sortByArray[0]]
-		else:
-			sortBy='TIMESTAMP'
-		for k in d.keys():
-			queryOptionsDict[k]=d[k]
-                queryDict=queryOptionsDict
-		for k in queryDict.keys():
-			if queryDict[k]==None or k not in queriables:
-				del queryDict[k]
-                if queryDict=={}:
-                        print('\n Full chain. If in interactive mode, please use rows.examples() \n')
-                        print sortBy
-			print orderBy
-			return self.df.sort_values(by='{}'.format(sortBy), ascending=orderBy)
-                newValues=['{0}=={1}{2}{1}'.format(key.upper(),'\\\'',value.upper()) for key,value in queryDict.items()]
-                newDict=dict(zip(queryDict.keys(),newValues))
-                if 'startTime' in queryDict.keys() and 'endTime' in queryDict.keys():
-                        newDict['TIMESTAMP']='{0}<=TIMESTAMP<={1}'.format(queryDict['startTime'],queryDict['endTime'])
-                        del newDict['startTime']
-                        del newDict['endTime']
-                array=['{}'.format(newDict.values()[i]) for i in range(0, len(newDict.values()))]
-                queryStr=' and '.join(array)
-                queryStr='\'{}\''.format(queryStr)
-                result=self.df.query('{}'.format(eval(queryStr)))
-		#print sortBy
-		#print orderBy
-                return result.sort_values(by='{}'.format(sortBy), ascending=orderBy)
-
+        df=pd.DataFrame(results, columns=columns)
+        if o.sortBy: df.sort_values(by=o.sortBy, ascending=(o.orderBy=='asc'), inplace=True)
+        return df
 
 if __name__=='__main__':
-        rows=Chain(o)
-
-
-#print (rows.firstQuery().to_string()) # If you want to show all rows and columns (not advised if you are querying something that appears in thousands of rows, i.e.; Node)
-print (rows.firstQuery())
-
-#	Memory Cost
-import os
-import psutil
-process = psutil.Process(os.getpid())
-print('\n\n Total memory in bytes:\n\n')
-print(process.memory_info().rss)
-
+        # this is an initial test parse; we'll reparse later if all is good
+        o=parser.parse_args()
+        
+        if len(sys.argv)>5: # non-interactive mode
+                df=Query(sys.argv[1:])
+                print(df)
+                print("\n{} rows".format(len(df)))
+                
+        else:
+                while True:
+                        q=raw_input('> ')
+                        # prepend the chain and stream args from the original invocation
+                        df=Query(sys.argv[1:]+q.split())
+                        print(df)
+                        print("\n{} rows".format(len(df)))
+                        
+                        
+#res=Query("-cn chain1 -sn stream1 -u 6 -sb TIMESTAMP")
+#print(res)
+#Query("-cn chain1 -sn stream1 -u 6 -i 5")
